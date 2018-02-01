a077443e621179a2571c31379f29fcf6f8f29974
Michał B <cyberwolf_96@outlook.com>
2018-02-01 21:28:57 +0100

    Added a few tests.

---
+++ AutomobileWebService.Test/CustomAttributes/CarNotValidDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xunit.Sdk;
+
+namespace AutomobileWebService.Tests.CustomAttributes
+{
+    class CarNotValidDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { "", -10, 0, new DateTime() };
+        }
+    }
+}
--- AutomobileWebService.Test/Models/CarShould.cs
+++ AutomobileWebService.Test/Models/CarShould.cs
@@ -1,3 +1,4 @@
+using AutomobileWebService.Business_Logic.Extras.Custom_Exceptions;
 using AutomobileWebService.Business_Logic.Models;
 using AutomobileWebService.Tests.CustomAttributes;
 using Moq;
@@ -8,6 +9,39 @@ namespace AutomobileWebService.Tests.Models
 {
     public class CarShould
     {
+
+        [Theory]
+        [CarValidData]
+        public void CreateItself(string model, int horsepower, int generation, DateTime productionDate)
+        {
+            //Arrange
+            var brandMock = Mock.Of<Brand>(x => x.Id == Guid.NewGuid() && x.Name == "Brand");
+            var sutId = Guid.NewGuid();
+
+            //Act
+            Car sut = new Car(sutId, model, horsepower, generation, productionDate, brandMock);
+
+            //Assert
+            Assert.Equal(sutId, sut.Id);
+            Assert.Equal(model, sut.Model);
+            Assert.Equal(brandMock.Id, sut.BrandId);
+            Assert.Equal(horsepower, sut.Horsepower);
+            Assert.Equal(generation, sut.Generation);
+            Assert.Equal(brandMock.Name, sut.BrandName);
+            Assert.Equal(productionDate, sut.ProdutionDate);
+        }
+
+        [Theory]
+        [CarNotValidData]
+        public void ThrowsExceptionDuringCreation(string model, int horsepower, int generation, DateTime productionDate)
+        {
+            //Arrange
+            var brandMock = Mock.Of<Brand>(x => x.Id == Guid.NewGuid() && x.Name == "Brand");
+
+            //Act and Assert
+            Assert.Throws<ForbiddenValueException>(() => new Car(Guid.NewGuid(),model, horsepower, generation, productionDate, brandMock));
+        }
+
         [Theory]
         [CarValidData]
         public void UpdateItself(string model, int horsepower, int generation, DateTime productionDate)
@@ -26,10 +60,16 @@ namespace AutomobileWebService.Tests.Models
             Assert.Equal(productionDate, sut.ProdutionDate);
         }
 
-        //[Theory]
-        //public void ThrowExceptionWhileUpdating()
-        //{
-            
-        //}
+        [Theory]
+        [CarNotValidData]
+        public void ThrowsExceptionDuringUpdate(string model, int horsepower, int generation, DateTime productionDate)
+        {
+            //Arrange
+            var brandMock = Mock.Of<Brand>(x => x.Id == Guid.NewGuid() && x.Name == "Brand");
+            Car sut = new Car(Guid.NewGuid(), "Test", 90, 1, new DateTime(1950, 1, 1), brandMock);
+
+            //Act and Assert
+            Assert.Throws<ForbiddenValueException>(() => sut.Update(model, horsepower, generation, productionDate));
+        }
     }
 }
