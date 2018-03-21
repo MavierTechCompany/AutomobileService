9dd21bfa72e9dedaf17ae3af2591a170384690c4
Trybic <ptyburczy@gmail.com>
2018-03-21 18:20:17 +0100

    Add blur (mobile) and darken (desktop),  when hamburger is open.

--- AutomobileWebService/wwwroot/js/Shared/layout.js
+++ AutomobileWebService/wwwroot/js/Shared/layout.js
@@ -18,30 +18,30 @@ function stickyNavigation() {
     }
 }
 
-window.addEventListener('scroll', stickyNavigation);
-
-
 //dropdown
 
+var userButton = document.getElementById('navbar__user-menu__container');
+var pageContent = document.getElementById('page-content');
+var hamburger = document.getElementById('hamburger');
+
+
 function dropdownMenu() {
-    let dropdownClosed = document.getElementById('dropdown--close');
-    let dropdownOpened = document.getElementById('dropdown--open');
+    let dropdownClosed = document.getElementById('dropdown--closed');
+    let dropdownOpened = document.getElementById('dropdown--opened');
     if(dropdownClosed){
-      dropdownClosed.id="dropdown--open";
-      document.getElementById('dropdown__content--close').id="dropdown__content--open";
+        document.getElementById("dropdown__content").style.cssText = null;
+        dropdownClosed.id="dropdown--opened";
     }
     else if(dropdownOpened){
-        dropdownOpened.id="dropdown--close";
-        document.getElementById('dropdown__content--open').id="dropdown__content--close";
+        document.getElementById("dropdown__content").style.display = "none";
+        dropdownOpened.id="dropdown--closed";
     }
 }
-
 
 //sidebar
 
 
-
-function SlideMenu(){
+function sideMenu(){
     let hamburgerClosed = document.getElementById('hamburger--closed');
     let hamburgerOpened = document.getElementById('hamburger--opened');
     let sidebar = document.getElementById('side-menu');
@@ -51,36 +51,38 @@ function SlideMenu(){
         hamburgerClosed.id="hamburger--opened";
         if (mobileWidth.matches) {
             sidebar.style.width = '100%';
+            pageContent.style.cssText ='filter: blur(3px);';
         }
         else {
             sidebar.style.width = '250px';
+            pageContent.style.cssText ='filter:brightness(40%); transition:0.4s;';
         }
     }
     else if (hamburgerOpened){
         hamburgerOpened.id="hamburger--closed";
         sidebar.style.width = '0';
+        pageContent.style.cssText = null;
     }
 }
 
 
-
-var hamburger = {
-    disabled: document.getElementById('hamburger--closed'),
-    active: document.getElementById('hamburger--opened'),
-    
+function dropdownMenuClose(){
+    let dropdownOpened = document.getElementById('dropdown--opened');
+    document.getElementById("dropdown__content").style.display = "none";
+    dropdownOpened.id="dropdown--closed";
 }
 
-
-
-
-
-function elementClose(){
+function sideMenuClose(){
     let hamburgerOpened = document.getElementById('hamburger--opened');
     let sidebar = document.getElementById('side-menu');
-    let dropdownOpened = document.getElementById('dropdown--open');
-
     hamburgerOpened.id="hamburger--closed";
     sidebar.style.width = '0';
-    dropdownOpened.id="dropdown--close";
-    document.getElementById('dropdown__content--open').id="dropdown__content--close";
+    pageContent.style.cssText = null;
 }
+
+
+window.addEventListener('scroll', stickyNavigation);
+userButton.onclick = dropdownMenu;
+hamburger.onclick = sideMenu;
+pageContent.onclick = sideMenuClose, dropdownMenuClose;
+// pageContent.onclick = 
