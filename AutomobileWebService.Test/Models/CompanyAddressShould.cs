514d107f10243f3ac799d54aa24c909da1fc14f9
Michał B <cyberwolf_96@outlook.com>
2018-04-02 11:00:19 +0200

    Unit test for CompanyAddress model class.

--- AutomobileWebService.Test/Models/CompanyAddressShould.cs
+++ AutomobileWebService.Test/Models/CompanyAddressShould.cs
@@ -44,5 +44,59 @@ namespace AutomobileWebService.Test.Models
             Assert.Throws<ForbiddenValueException>(() => new CompanyAddress(country, state,
                 town, street, buildingNumber, flatNumber, zipCode, companyMock));
         }
+
+        [Theory]
+        [CompanyAddressValidData]
+        public void UpdateItself(string country, string state, string town, string street,
+            int buildingNumber, int flatNumber, string zipCode)
+        {
+            //Arrange
+            var companyMock = Mock.Of<Company>(x => x.Id == 1);
+            var sut = new CompanyAddress("Test Country", "Test State", "Test Town",
+                "Test Street", 87, 8, "00-000", companyMock);
+
+            //Act
+            sut.Update(country, state, town, street, buildingNumber,
+                flatNumber, zipCode);
+
+            //Assert
+            Assert.Equal(country, sut.Country);
+            Assert.Equal(state, sut.State);
+            Assert.Equal(town, sut.Town);
+            Assert.Equal(street, sut.Street);
+            Assert.Equal(buildingNumber, sut.BuildingNumber);
+            Assert.Equal(flatNumber, sut.FlatNumber);
+            Assert.Equal(zipCode, sut.ZipCode);
+        }
+
+        [Theory]
+        [CompanyAddressNotValidData]
+        public void ThrowsExceptionDuringUpdate(string country, string state, string town,
+            string street, int buildingNumber, int flatNumber, string zipCode)
+        {
+            //Arrange
+            var companyMock = Mock.Of<Company>(x => x.Id == 1);
+            var sut = new CompanyAddress("Test Country", "Test State", "Test Town",
+                "Test Street", 87, 8, "00-000", companyMock);
+
+            //Act and Assert
+            Assert.Throws<ForbiddenValueException>(() => sut.Update(country, state, town,
+                street, buildingNumber, flatNumber, zipCode));
+        }
+
+        [Fact]
+        public void DeleteItself()
+        {
+            //Arrange
+            var companyMock = Mock.Of<Company>(x => x.Id == 1);
+            var sut = new CompanyAddress("Test Country", "Test State", "Test Town",
+                "Test Street", 87, 8, "00-000", companyMock);
+
+            //Act
+            sut.Delete();
+
+            //Assert
+            Assert.True(sut.Deleted);
+        }
     }
 }
